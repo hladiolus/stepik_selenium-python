89da6ccc86d9d6fc390d88178c8c75a3a101f769
Bohdan <33325944+hladiolus@users.noreply.github.com>
2021-04-21 11:58:06 +0300

    Update 3213.py

--- 3213.py
+++ 3213.py
@@ -3,22 +3,25 @@ import unittest
 import time
 
 class TestAbs(unittest.TestCase):
-    def tests(self):
-        
-        def test_form(url):
-            browser = webdriver.Chrome()
-            browser.get(url)
-            browser.find_element_by_css_selector(".first_block .first_class input").send_keys("Ivan")
-            browser.find_element_by_css_selector(".first_block .second_class input").send_keys("Petrov")
-            browser.find_element_by_css_selector(".first_block .third_class input").send_keys("qwe@gmail.com")
+    def test1(self):
+        browser = webdriver.Chrome()
+        browser.get("http://suninjuly.github.io/registration1.html")
+        browser.find_element_by_css_selector(".first_block .first_class input").send_keys("Ivan")
+        browser.find_element_by_css_selector(".first_block .second_class input").send_keys("Petrov")
+        browser.find_element_by_css_selector(".first_block .third_class input").send_keys("qwe@gmail.com")
+        browser.find_element_by_xpath("//button[text()[contains(.,'Submit')]]").click()
+        time.sleep(3)
+        self.assertEqual(u"Congratulations! You have successfully registered!", browser.find_element_by_tag_name("h1").text)
 
-            browser.find_element_by_xpath("//button[text()[contains(.,'Submit')]]").click()
-            time.sleep(3)
-            welcome = browser.find_element_by_tag_name("h1").text
-            self.assertEqual(u"Congratulations! You have successfully registered!", welcome)
-
-        test_form("http://suninjuly.github.io/registration1.html")
-        test_form("http://suninjuly.github.io/registration2.html")
+    def test2(self):
+        browser = webdriver.Chrome()
+        browser.get("http://suninjuly.github.io/registration2.html")
+        browser.find_element_by_css_selector(".first_block .first_class input").send_keys("Ivan")
+        browser.find_element_by_css_selector(".first_block .second_class input").send_keys("Petrov")
+        browser.find_element_by_css_selector(".first_block .third_class input").send_keys("qwe@gmail.com")
+        browser.find_element_by_xpath("//button[text()[contains(.,'Submit')]]").click()
+        time.sleep(3)
+        self.assertEqual(u"Congratulations! You have successfully registered!", browser.find_element_by_tag_name("h1").text)
 
 
 if __name__ == "__main__": 
